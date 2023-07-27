5b72102fefea8f937d2235db974eb9708c2cf840
admalla <maasovec@gmail.com>
2023-07-27 15:24:04 +0300

    fix component Posts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import Post from "./Post/Post";
 
 function MyPosts() {
+    const postData = [
+        {id: 1, message: 'hello world', like: 15},
+        {id: 2, message: 'It is my first post', like: 20}
+    ]
     return (
         <div>
-            <Post like={15} message="hello world"/>
-            <Post like={20} message="It's my first post"/>
+            {postData.map(p => {
+                return <Post key={p.id} message={p.message} like={p.like}/>
+            })}
         </div>
     );
 }
